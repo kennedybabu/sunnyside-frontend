238e2eee85f79a5fc05067ee2608f33e97b971d9
kennedybabu <kennedynjogu8@gmail.com>
2022-06-25 12:09:05 +0300

    fix: use the arrow in the hero section to scroll down when clicked

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Arrow from '../assets/icon-arrow-down.svg'
+import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
@@ -8,7 +9,9 @@ const Hero = () => {
         <div className='flex items-center justify-center h-full w-full flex-col'>
                 <h2 className='text-header text-3xl md:text-6xl uppercase text-[Fraunces] text-white'>We are creatives</h2>
                 <div className='pt-[50px] pointer cursor-pointer'>
+                  <Link to='about' smooth={true}>
                     <img src={Arrow} alt="arrow down svg" />
+                  </Link>
                 </div>
         </div>
     </div>
